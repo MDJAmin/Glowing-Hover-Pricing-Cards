a8aa11d43949768b66ae8d10571eccc368a94c92
M_Αmin Jafarnejad <mohammadamindf@gmail.com>
2024-10-02 09:39:02 +0330

    Feat : some hovering functionality

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ const cardsContainerInner = document.querySelector(".cards__inner");
 const cards = Array.from(document.querySelectorAll(".card"));
 const overlay = document.querySelector(".overlay");
 
+const applyOverlayMask = (e) => {
+    const overlayEl = e.currentTarget;
+    const x = e.pageX - cardsContainer.offsetLeft;
+    const y = e.pageY - cardsContainer.offsetTop;
+  
+    overlayEl.style = `--opacity: 1; --x: ${x}px; --y:${y}px;`;
+  };
+  
+const createOverlayCta = (overlayCard, ctaEl) => {
+    const overlayCta = document.createElement("div");
+    overlayCta.classList.add("cta");
+    overlayCta.textContent = ctaEl.textContent;
+    overlayCta.setAttribute("aria-hidden", true);
+    overlayCard.append(overlayCta);
+  };
+  
