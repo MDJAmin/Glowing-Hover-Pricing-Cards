923f56f940702278016e0b567cb9449120009d34
M_Αmin Jafarnejad <mohammadamindf@gmail.com>
2024-10-02 09:38:02 +0330

    Feat : get some El from dom

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 "use strict"
+
+const cardsContainer = document.querySelector(".cards");
+const cardsContainerInner = document.querySelector(".cards__inner");
+const cards = Array.from(document.querySelectorAll(".card"));
+const overlay = document.querySelector(".overlay");
+
